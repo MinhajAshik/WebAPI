361278d067e5d7df6d43e2ef49347751cfb721f0
MotaAguero <minhajulislama@gmail.com>
2022-06-07 23:49:39 +0600

    ReserveIt WebApi Assessment

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+      
+        private readonly Data _data;
+        private string headerKey = "page-size";
+
+        public ValuesController(Data data)
+        {
+            this._data = data;
+        }
+
+        [HttpPost("save")]
+        public IActionResult SaveWords([FromBody] List<string> words)
+        {
+            Request.Headers.TryGetValue(headerKey, out var headerValue);
+            _data.PageSize = Convert.ToInt32(headerValue);
+            _data.Words = words;
+
+            return Ok();
+
+
+        }
+
+        [HttpGet("get")]
+        public ActionResult<IEnumerable<string>> GetWords()
+        {
+            var result = string.Empty;
+            foreach (var item in _data.Words)
+            {
+                var maxLength = Math.Min(item.Length, _data.PageSize);
+                result += item.Substring(0, maxLength)+"\r\n";
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ WebApi/Data.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebApi
+{
+    public class Data
+    {
+        public int PageSize { get; set; }
+        public List<string> Words { get; set; }
+        
+    }
+}
